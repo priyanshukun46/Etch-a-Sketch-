9936541ce4c22294be3a052d10cb0f96d427e180
priyanshukun46 <pkfb46@gmail.com>
2026-06-02 00:07:36 +0530

    v:-> 0.30 ; done with html and css and dyanamic boxing done :D

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Etch-a-Sketch ");
+let input = prompt("number of boxes : ");
+let app = document.querySelector(".app");
+const z = input*input;
+const w = 500 / input;
+const h = 500 / input;
+
+
+for (let i = 0; i < z; i++){
+  let div = document.createElement("div");
+  div.style.border = "1px solid black";
+  div.style.width = `${w}px`;
+  div.style.height = `${h}px`;
+  div.className = "innerbox";
+  app.appendChild(div);
+  
+}
